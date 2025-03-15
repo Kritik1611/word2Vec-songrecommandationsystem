7e3a32fbaef26d1a3867d7e22238fa0c16d0cae4
Kritik1611 <143782491+Kritik1611@users.noreply.github.com>
2025-03-15 21:55:34 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,20 @@
+Recommending songs by embeddings
+import pandas as pd
+from urllib import request
 
+# Get the playlist dataset file
+data = request.urlopen('https://storage.googleapis.com/maps-premium/dataset/yes_complete/train.txt')
+
+# Parse the playlist dataset file. Skip the first two lines as
+# they only contain metadata
+lines = data.read().decode("utf-8").split('\n')[2:]
+
+# Remove playlists with only one song
+playlists = [s.rstrip().split() for s in lines if len(s.split()) > 1]
+
+# Load song metadata
+songs_file = request.urlopen('https://storage.googleapis.com/maps-premium/dataset/yes_complete/song_hash.txt')
+songs_file = songs_file.read().decode("utf-8").split('\n')
+songs = [s.rstrip().split('\t') for s in songs_file]
+songs_df = pd.DataFrame(data=songs, columns = ['id', 'title', 'artist'])
+songs_df = songs_df.set_index('id')
